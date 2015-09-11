cdf4b31ced6d394fc4209cd93dd2cb5bab9b5e6a
Nadiar AS <nadiar429@gmail.com>
2015-09-11 21:48:43 +0700

    adding middleware to bookRoute.js

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -28,26 +28,30 @@ var routes = function(Book) {
 			});
 		});
 
+	bookRouter.use('/:bookId', function(req, res, next) {
+		Book.findById(req.params.bookId, function(err, book) {
+			if (err) {
+				res.status(500).send(err);
+			} else if (book) {
+				req.book = book;
+				next();
+			} else {
+				res.status(404).send('book not found');
+			}
+		});
+	});
+
 	bookRouter.route('/:bookId')
 		.get(function(req, res) {
-			Book.findById(req.params.bookId, function(err, book) {
-				if (err)
-					res.status(500).send(err);
-				else
-					res.json(book);
-			});
+			res.json(req.book);
 		})
 		.put(function(req, res) {
-			Book.findById(req.params.bookId, function(err, book) {
-				if (err)
-					res.status(500).send(err);
-				else
-					book.title = req.body.title;
-					book.author = req.body.author;
-					book.genre = req.body.genre;
-					book.save();
-					res.json(book);
-			});
+			req.book.title = req.body.title;
+			req.book.author = req.body.author;
+			req.book.genre = req.body.genre;
+			req.book.read = req.body.read;
+			req.book.save();
+			req.json(req.book);
 		});
 
 	return bookRouter;
